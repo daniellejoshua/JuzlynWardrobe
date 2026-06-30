482cf4d0d91bc83c5ae41853967f9b113b35616b
Danielle Joshua Esternon <daniellejoshua18@gmail.com>
2026-06-30 11:58:00 +0800

    ADD: model serivece,endpoints,database

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,13 @@
 from dotenv import load_dotenv
+
+
 load_dotenv("../.env")
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.api.routes import upload,generate,outfits,tryon
+from app.api.routes import generate,outfits,tryon,outfit_upload
+from app.api.routes.models import getModels,model_upload
+
 
 app = FastAPI(title="AI WARDROBE STYLIST API")
 
@@ -17,10 +21,16 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(upload.router, prefix="/upload", tags=["Upload"])
+app.include_router(outfit_upload.router, prefix="/upload", tags=["Upload"])
 app.include_router(generate.router, prefix="/generate", tags=["Generate"])
 app.include_router(outfits.router,prefix="/outfits",tags=["getAllOutfits"])
 app.include_router(tryon.router,prefix="/tryon",tags=["tryonCombo"])
+app.include_router(getModels.router, prefix="/models")
+app.include_router(model_upload.router, prefix="/models/upload")
+
+
+
+
 
 @app.get("/")
 def root():
