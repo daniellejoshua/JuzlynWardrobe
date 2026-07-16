51d71286f15b6477604f6b1b9908d9bdb6f34e83
Danielle Joshua Esternon <daniellejoshua18@gmail.com>
2026-07-16 21:23:27 +0800

    del some unsude imports

--- frontend/components/use-queries.ts
+++ frontend/components/use-queries.ts
@@ -1,8 +1,5 @@
 import { generateCombinations, getModels, getOutfits, tryOnCombo, uploadModel, uploadOutfit, } from "@/lib/api";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-import { Outfit } from "next/font/google";
-import { use } from "react";
-import { fromTheme } from "tailwind-merge";
 
 
 // ── Queries ──
